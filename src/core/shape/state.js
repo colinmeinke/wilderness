145172769427090aba75931fadfcfc32a460bbfa
Colin Meinke <hello@colinmeinke.com>
2016-07-19 16:17:49 +0100

    fix(state): should not throw error if no secondary shape

--- src/core/shape/state.js
+++ src/core/shape/state.js
@@ -2,6 +2,18 @@ import { currentState, easingFunc, normalise, tween } from '../helpers';
 import { stylePropAttrMap } from './props';
 import { toPath } from 'svg-points';
 
+const animationState = ( state, timeline ) => {
+  const { keyframes, timing } = timeline;
+  const currentAnimation = state && state.animation ? state.animation : {};
+  const animation = { ...currentAnimation, ...currentState( currentAnimation )};
+  const { currentProgress } = animation;
+
+  return {
+    ...animation,
+    ...currentKeyframes( currentProgress, keyframes, timing ),
+  };
+};
+
 const currentKeyframes = ( currentProgress, keyframes, timing ) => {
   const keyframe1Index = timing.reduce(( a, b, i ) => currentProgress > b ? i : a, 0 );
   const keyframe2Index = keyframe1Index + 1;
@@ -11,26 +23,38 @@ const currentKeyframes = ( currentProgress, keyframes, timing ) => {
   return { keyframe1, keyframe1Index, keyframe2, keyframe2Index };
 };
 
-const frameShapes = ({
+const currentShapes = ({
   currentProgress,
   duration,
   easing,
-  keyframe1,
-  keyframe2,
+  shapes1,
+  shapes2,
   time,
 }) => {
-  if ( currentProgress === 0 || !keyframe2 ) {
-    return keyframe1.shapes;
-  } else if ( currentProgress === 1 ) {
-    return keyframe2.shapes;
-  }
+  const [ s1, s2 ] = normalisedShapes( shapes1, shapes2 );
 
-  const [ shapes1, shapes2 ] = shapes( keyframe1, keyframe2 );
-
-  return shapes1.map(( s1, i ) => {
-    const s2 = shapes2[ i ];
-    return tween( s1, s2, time, duration, easing );
+  return s1.map(( a, i ) => {
+    const b = s2[ i ];
+    return tween( a, b, time, duration, easing );
   });
+};
+
+const normalisedShapes = ( shapes1, shapes2 ) => {
+  const a = [];
+  const b = [];
+
+  shapes1.map(( shape1, i ) => {
+    const shape2 = shapes2[ i ];
+
+    const [ s1, s2 ] = shape1.points ?
+      normalise( shape1, shape2 ) :
+      [ shape1, shape2 ];
+
+    a.push( s1 );
+    b.push( s2 );
+  });
+
+  return [ a, b ];
 };
 
 const shapeAttributes = ({ points, styles }) => {
@@ -41,24 +65,6 @@ const shapeAttributes = ({ points, styles }) => {
   }
 
   return attributes;
-};
-
-const shapes = ( keyframe1, keyframe2 ) => {
-  const shapes1 = [];
-  const shapes2 = [];
-
-  keyframe1.shapes.map(( shape1, i ) => {
-    const shape2 = keyframe2.shapes[ i ];
-
-    const [ s1, s2 ] = shape1.points ?
-      normalise( shape1, shape2 ) :
-      [ shape1, shape2 ];
-
-    shapes1.push( s1 );
-    shapes2.push( s2 );
-  });
-
-  return [ shapes1, shapes2 ];
 };
 
 const styleAttributes = styles => {
@@ -75,53 +81,59 @@ const styleAttributes = styles => {
   return s;
 };
 
-const state = shape => {
-  const { keyframes, timing } = shape.timeline;
-
-  const currentAnimation = shape.state && shape.state.animation ?
-    shape.state.animation : {};
-
-  const animation = { ...currentAnimation, ...currentState( currentAnimation )};
-  const { currentProgress } = animation;
-
+const shapes = ( animation, timing ) => {
   const {
+    currentProgress,
+    easing: defaultEasing,
     keyframe1,
     keyframe1Index,
     keyframe2,
     keyframe2Index,
-  } = currentKeyframes( currentProgress, keyframes, timing );
+  } = animation;
+
+  if ( currentProgress === 0 || !keyframe2 ) {
+    return keyframe1.shapes;
+  } else if ( currentProgress === 1 ) {
+    return keyframe2.shapes;
+  }
 
   const scale = timing[ keyframe2Index ] - timing[ keyframe1Index ];
   const offset = currentProgress - timing[ keyframe1Index ];
   const duration = animation.duration * scale;
   const time = duration * offset / scale;
-  const easing = keyframe2.animation.easing || animation.easing;
+  const easing = keyframe2.animation.easing || defaultEasing;
 
-  const shapes = frameShapes({
+  return currentShapes({
     currentProgress,
     duration,
     easing: typeof easing === 'function' ? easing : easingFunc( easing ),
-    keyframe1,
-    keyframe2,
+    shapes1: keyframe1.shapes,
+    shapes2: keyframe2.shapes,
     time,
-  }).map( shapeAttributes );
-
-  const state = {
-    animation: { ...animation, keyframe1Index, keyframe2Index },
-    shapes,
-  };
-
-  shape.state = state;
-
-  return state;
+  });
 };
 
 export {
+  animationState,
   currentKeyframes,
-  frameShapes,
+  currentShapes,
+  normalisedShapes,
   shapeAttributes,
   shapes,
-  styleAttributes,
 };
 
-export default state;
+export default shape => {
+  const { state, timeline } = shape;
+  const { timing } = timeline;
+
+  const animation = animationState( state, timeline );
+
+  const s = {
+    animation,
+    shapes: shapes( animation, timing ).map( shapeAttributes ),
+  };
+
+  shape.state = s;
+
+  return s;
+};
