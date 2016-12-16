c5cefb04f7e6359e5305220ab6e07daca7e39344
Colin Meinke <hello@colinmeinke.com>
2016-12-16 14:17:26 +0100

    test(core): document and test match helper

--- src/core/helpers/match.js
+++ src/core/helpers/match.js
@@ -1,9 +1,24 @@
-const match = (a, b, f) => {
+/**
+ * Calls a function on each matching item from two sets of data.
+ *
+ * @param a - The first set of data.
+ * @param b - The second set of data. Must be of an identicle structure to a.
+ * @param {function} func - The function to call on the data.
+ *
+ * @example
+ * // returns 3
+ * match(1, 2, (a, b) => a + b)
+ *
+ * @example
+ * // returns [ 4, 6 ]
+ * match([ 1, 2 ], [ 3, 4 ], (a, b) => a + b)
+ */
+const match = (a, b, func) => {
   if (Array.isArray(a) && Array.isArray(b)) {
     const result = []
 
     for (let i = 0, l = a.length; i < l; i++) {
-      result.push(match(a[ i ], b[ i ], f))
+      result.push(match(a[ i ], b[ i ], func))
     }
 
     return result
@@ -11,13 +26,13 @@ const match = (a, b, f) => {
     const result = {}
 
     for (let k of Object.keys(a)) {
-      result[ k ] = match(a[ k ], b[ k ], f)
+      result[ k ] = match(a[ k ], b[ k ], func)
     }
 
     return result
   }
 
-  return f(a, b)
+  return func(a, b)
 }
 
 export default match
---
+++ tests/core/helpers/match.js
@@ -0,0 +1,38 @@
+/* globals describe it expect */
+
+import match from '../../../src/core/helpers/match'
+
+const add = (a, b) => a + b
+
+describe('match', () => {
+  it('applies function correctly to string data', () => {
+    expect(match('foo', 'bar', add)).toBe('foobar')
+  })
+
+  it('applies function correctly to number data', () => {
+    expect(match(1, 2, add)).toBe(3)
+  })
+
+  it('applies function correctly to array data', () => {
+    expect(match([ 1, 2 ], [ 3, 4 ], add)).toEqual([ 4, 6 ])
+  })
+
+  it('applies function correctly to nested array data', () => {
+    expect(match([ [ 1 ], [ [ 2 ], 3 ] ], [ [ 4 ], [ [ 5 ], 6 ] ], add))
+      .toEqual([ [ 5 ], [ [ 7 ], 9 ] ])
+  })
+
+  it('applies function correctly to object data', () => {
+    expect(match({ foo: 1 }, { foo: 2 }, add)).toEqual({ foo: 3 })
+  })
+
+  it('applies function correctly to nested object data', () => {
+    expect(match({ a: { b: 1 }, c: { d: { e: 2 }}}, { a: { b: 3 }, c: { d: { e: 4 }}}, add))
+      .toEqual({ a: { b: 4 }, c: { d: { e: 6 }}})
+  })
+
+  it('applies function correctly to nested mixed data', () => {
+    expect(match({ foo: 1, bar: [ 2, 'a' ]}, { foo: 3, bar: [ 4, 'b' ]}, add))
+      .toEqual({ foo: 4, bar: [ 6, 'ab' ]})
+  })
+})
