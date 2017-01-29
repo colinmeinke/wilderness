a85ad68fb68093e0a73c16d62702caafc77ca1f6
Colin Meinke <hello@colinmeinke.com>
2017-01-29 11:20:47 +0100

    chore: fix lint errors

--- examples/fill-rule/client.js
+++ examples/fill-rule/client.js
@@ -9,27 +9,27 @@ const from1 = {
 
 const from2 = {
   type: 'path',
-  d: 'M20,120H80V180H20ZM30,130V170H70ZM40,140H60V160H40Z',
+  d: 'M20,120H80V180H20ZM30,130V170H70ZM40,140H60V160H40Z'
 }
 
 const from3 = {
   type: 'path',
-  d: 'M20,220H80V280H20ZM30,230V270H70ZM40,240H60V260H40ZM35,250V265H50Z',
+  d: 'M20,220H80V280H20ZM30,230V270H70ZM40,240H60V260H40ZM35,250V265H50Z'
 }
 
 const to1 = {
   type: 'path',
-  d: 'M290,40h20v20h-20z',
+  d: 'M290,40h20v20h-20z'
 }
 
 const to2 = {
   type: 'path',
-  d: 'M290,140h20v20h-20z',
+  d: 'M290,140h20v20h-20z'
 }
 
 const to3 = {
   type: 'path',
-  d: 'M290,240h20v20h-20z',
+  d: 'M290,240h20v20h-20z'
 }
 
 const animation1 = shape(from1, to1)
