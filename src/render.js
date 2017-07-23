b02bd17e1326f274f0d1e6826e7541dd2f1bf7d0
Colin Meinke <hello@colinmeinke.com>
2017-07-23 17:45:16 -0500

    feat(timeline): add validation, code comments and tests

--- src/render.js
+++ src/render.js
@@ -4,6 +4,24 @@ import { frame } from 'wilderness-core'
 import { node } from 'wilderness-dom-node'
 import { tick } from './timeline'
 
+/**
+ * An object holding both Shapes and Timelines.
+ *
+ * @typedef {Object} ShapesAndTimelines
+ *
+ * @property {Shape[]} shapes
+ * @property {Timeline[]} timelines
+ */
+
+/**
+ * Appends or replaces a Shape Node within a container Node.
+ *
+ * @param {Node} container
+ * @param {Shape} shape
+ *
+ * @example
+ * appendNode(svg, shape)
+ */
 const appendNode = (container, shape) => {
   if (shape.replace) {
     shape.replace.parentNode.replaceChild(shape.node, shape.replace)
@@ -13,14 +31,32 @@ const appendNode = (container, shape) => {
   }
 }
 
+/**
+ * Creates a Node for a Shape given a FrameShape.
+ *
+ * @param {Shape} shape
+ * @param {FrameShape} frameShape
+ *
+ * @example
+ * createNode(shape, frameShape)
+ */
 const createNode = (shape, frameShape) => {
   shape.node = node(frameShape)
 }
 
+/**
+ * Renders Shapes or Timelines to a container Node.
+ *
+ * @param {Node} container
+ * @param {...(Shape|Timeline)} shapesAndTimelines
+ *
+ * @example
+ * render(svg, shape, timeline)
+ */
 const render = (container, ...shapesAndTimelines) => {
   if (__DEV__) {
     if (typeof container !== 'object' || !container.nodeName) {
-      throw new TypeError(`The first argument passed to the render function must be a dom node`)
+      throw new TypeError(`The render function must be a DOM node as first argument`)
     }
   }
 
@@ -38,7 +74,10 @@ const render = (container, ...shapesAndTimelines) => {
     })
   })
 
-  shapes.map(shape => appendNode(container, shape))
+  shapes.map(shape => {
+    appendNode(container, shape)
+    shape.rendered = true
+  })
 
   timelines.map(timeline => {
     timeline.timelineShapes.map(({ shape }) => appendNode(container, shape))
@@ -48,20 +87,41 @@ const render = (container, ...shapesAndTimelines) => {
   tick()
 }
 
+/**
+ * Splits a Shape and Timeline array into ShapesAndTimelines.
+ *
+ * @param {(Shape|Timeline)[]} shapesAndTimelines
+ *
+ * @returns {ShapesAndTimelines}
+ *
+ * @example
+ * split([ shape, timeline ])
+ */
 const split = shapesAndTimelines => {
   const shapes = []
   const timelines = []
 
   shapesAndTimelines.map(x => {
-    if (x.keyframes) {
-      if (__DEV__ && x.timeline) {
-        throw new Error(`You cannot render a shape that has been placed on a timeline, instead render the timeline`)
+    if (typeof x === 'object' && x.keyframes) {
+      if (__DEV__) {
+        if (x.timeline) {
+          throw new Error(`You cannot render a shape that has been placed on a timeline, instead render the timeline`)
+        }
+
+        if (x.rendered) {
+          throw new Error(`You cannot render the same shape twice`)
+        }
       }
 
       shapes.push(x)
-    } else {
-      // @todo validate timeline
+    } else if (typeof x === 'object' && x.middleware && x.playbackOptions && x.state && x.timelineShapes) {
+      if (__DEV__ && x.state.rendered) {
+        throw new Error(`You cannot render the same timeline twice`)
+      }
+
       timelines.push(x)
+    } else if (__DEV__) {
+      throw new Error(`The render function only takes shapes and timelines from the second argument onwards`)
     }
   })
 
